fceacf54610e22ec2984b5576c6339ba7c495505
unknown <SmForWorld@gmail.com>
2014-09-18 00:43:20 +0530

    first commit

---
+++ AngularCustomerApps/app.js
@@ -0,0 +1,21 @@
+﻿var CustomerApp = angular.module("CustomerApp", ['ngRoute']);
+
+CustomerApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('customers',
+            {
+                controller: '',
+                templateUrl:''
+            })
+            .when('/customerOrders',
+            {
+                controller: '',
+                templateUrl:''
+            })
+            .when('/orders',
+            {
+                controller: '',
+                templateUrl:''
+            }
+        )
+    });
